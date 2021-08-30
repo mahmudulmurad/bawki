31d4c6440cdaeaa28b317ec3f4a7d44a3c8dfe93
mahmudulmurad <mahmudul.murad7@gmail.com>
2021-08-30 18:24:42 +0600

    multiple user added error fixed

--- backend/routes/conversationRouter.js
+++ backend/routes/conversationRouter.js
@@ -3,40 +3,52 @@ const Conversation = require("../models/conversationModel")
 const auth = require('../auth/auth')
 const User = require("../models/userModel")
 
+
 // create conversation
 router.post('/createConversation', auth, async (req, res) => {
-    try {
-        const { receiverId } = req.body
-        const shakeHand = await new Conversation({
-            members: [req.user._id, receiverId]
-        })
+  try {
+    const { receiverId } = req.body
+    let isExists = await Conversation.find({ members: { $in: [receiverId] } })
 
-          let otherUser = await User.findOneAndUpdate({ _id: receiverId },
-          { $push: { friends: req.user._id } }, { new: true }).exec()
+    if (isExists.length > 0) {
 
-          let otherUser2 = await User.findOneAndUpdate({ _id: req.user._id },
-            { $push: { friends: receiverId } }, { new: true }).exec()
+      res.status(403).json({
+        status: false,
+        error: 'conversation already exists'
+      })
 
-        await shakeHand.save()
-        await otherUser.save()
-        await otherUser2.save()
-
-        res.status(201).json(shakeHand)
-    } catch (error) {
-        res.status(500).json(error.message)
     }
+    else {
+
+      const shakeHand = await new Conversation({
+        members: [req.user._id, receiverId]
+      })
+
+      await shakeHand.save()
+
+      await User.findOneAndUpdate({ _id: receiverId },
+        { $addToSet: { friends: req.user._id } }, { new: true }).exec()
+
+      await User.findOneAndUpdate({ _id: req.user._id },
+        { $addToSet: { friends: receiverId } }, { new: true }).exec()
+      res.status(201).json(shakeHand)
+    }
+
+  } catch (error) {
+    res.status(500).json(error.message)
+  }
 })
 
 // get user conversation
-router.get("/conversation/:userId",auth, async (req, res) => {
-    try {
-      const conversation = await Conversation.find({
-        members: { $in: [req.params.userId] },
-      });
-      res.status(200).json(conversation);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-  
+router.get("/conversation/:userId", auth, async (req, res) => {
+  try {
+    const conversation = await Conversation.find({
+      members: { $in: [req.params.userId] },
+    });
+    res.status(200).json(conversation);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router
